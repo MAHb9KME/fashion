ae1dca51018dadadb0dbeac2c61d7e22ed5bc99d
MAHb9KME <rinat.zaripov.2011@mail.ru>
2020-10-16 10:24:59 +0300

    Загрузил итоговым шаблоном

---
+++ index.php
@@ -0,0 +1,148 @@
+﻿<!DOCTYPE html>
+<html lang="ru-RU" >
+<head>
+	<title>Образовательный портал</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="Cache-control" content="public"> 
+	<link rel="stylesheet" href="style.css" />
+	<script type='text/javascript' src='js/jquery.js'></script>
+	<link rel="shortcut icon" type="image/png" href="favicon.png" />
+</head>
+
+<body>
+
+
+	<div class="wr-materials ht-distance">
+		<div class="container">
+			<div class="title">Популярные материалы</div>
+			<div class="materials">
+				<div class="mat-navigate">
+					<ul class="xs_flex xs_start">
+						<li><a href="#" class="active">Все материалы</a></li>
+						<li><a href="#">Видео</a></li>
+						<li><a href="#">Аудио</a></li>
+						<li><a href="#">Статьи</a></li>
+					</ul>
+				</div>
+
+				<div class="mat-content xs_flex">
+					<div class="item">
+						<a href="#" class="photo" style="background: url('images/pics/pop2.jpg') no-repeat 50% 50%; background-size: cover;"></a>
+
+						<div class="type-icon audio">
+							<span>Аудио</span>
+						</div>
+
+						<a href="#" class="item-title">
+							5 джинсовых трендов осени 2020
+						</a>
+
+						<div class="info">
+							<div class="identity xs_flex xs_middle">
+								<div class="info-txt">
+									<div class="name">Миронова Анастасия</div>
+									<div class="price xs_flex xs_start xs_middle">
+										<div class="new">690 руб</div>
+										<div class="old">1 220 руб</div>
+									</div>
+								</div>
+								<div class="avatar">
+									<img src="images/pics/avatar_2.png" alt="">
+								</div>
+							</div>
+
+							<div class="info-action xs_flex xs_middle xs_start">
+								<a href="#" class="btn">Подробнее</a>
+								<a href="#" class="btn_action">Купить</a>
+							</div>
+						</div>
+					</div>
+
+					<div class="item">
+						<a href="#" class="photo" style="background: url('images/pics/pop3.jpg') no-repeat 50% 50%; background-size: cover;"></a>
+
+						<div class="type-icon video">
+							<span>видео</span>
+						</div>
+
+						<a href="#" class="item-title">
+							Мода америки: традиции и современность
+						</a>
+
+						<div class="info">
+							<div class="identity xs_flex xs_middle">
+								<div class="info-txt">
+									<div class="name">Миронова Анастасия</div>
+									<div class="price xs_flex xs_start xs_middle">
+										<div class="new">690 руб</div>
+										<div class="old">1 220 руб</div>
+									</div>
+								</div>
+								<div class="avatar">
+									<img src="images/pics/avatar_2.png" alt="">
+								</div>
+							</div>
+
+							<div class="info-action xs_flex xs_middle xs_start">
+								<a href="#" class="btn">Подробнее</a>
+								<a href="#" class="btn_action">Купить</a>
+							</div>
+						</div>
+					</div>
+
+					<div class="item">
+						<a href="#" class="photo" style="background: url('images/pics/pop4.jpg') no-repeat 50% 50%; background-size: cover;"></a>
+
+						<div class="type-icon articles">
+							<span>статья</span>
+						</div>
+
+						<a href="#" class="item-title">
+							Визуальные коммуникации брендов: от кампейнов до магазинов
+						</a>
+
+						<div class="info">
+							<div class="identity xs_flex xs_middle">
+								<div class="info-txt">
+									<div class="name">Миронова Анастасия</div>
+									<div class="price xs_flex xs_start xs_middle">
+										<div class="new">690 руб</div>
+										<div class="old">1 220 руб</div>
+									</div>
+								</div>
+								<div class="avatar">
+									<img src="images/pics/avatar_2.png" alt="">
+								</div>
+							</div>
+
+							<div class="info-action xs_flex xs_middle xs_start">
+								<a href="#" class="btn">Подробнее</a>
+								<a href="#" class="btn_action">Купить</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<a href="#" class="btn_link">Показать все популярные материалы</a>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+
+	<link rel="stylesheet" href="css/media.css" />
+	<link rel="stylesheet" href="css/slick.css" />
+	<link rel="stylesheet" href="fonts/fonts.css" />
+	<link rel="stylesheet" href="css/jquery.fancybox.css" />
+	<script src="js/slick.js"></script>
+	<script src="js/jquery.fancybox.js"></script>
+	<script src="js/jquery.inputmask.js"></script>
+	<script src="js/template.js"></script>
+
+</body>
+</html>
+
